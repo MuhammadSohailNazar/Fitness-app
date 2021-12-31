35a3b1e58f6b98341a0d9ce0d6d52fead170d469
Muhammad Sohail Nazar <sohail@tcig.co>
2021-12-31 23:36:33 +0300

    login and register changes.

--- src/app/containers/app/app.component.ts
+++ src/app/containers/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Observable, Subscription } from "rxjs";
 
+import { Store } from "store";
+
+import { AuthService, User } from "../../../auth/shared/services/auth/auth.service";
 @Component({
-  selector: 'app-root',
-  styleUrls: ['app.component.scss'],
-  templateUrl: 'app.component.html'
+  selector: "app-root",
+  styleUrls: ["app.component.scss"],
+  templateUrl: "app.component.html",
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+
+  user$: Observable<User>;
+  subscription: Subscription;
+  constructor(private store: Store, private authSerivce: AuthService) {}
+
+  ngOnInit() {
+    this.subscription = this.authSerivce.auth$.subscribe();
+    this.user$ = this.store.select<User>("user");
+  } 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
