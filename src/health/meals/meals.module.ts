91138e11273d3ea9da7efcd0c8a40ed34c3c6019
Muhammad Sohail Nazar <sohail@tcig.co>
2022-01-01 13:28:49 +0300

    adding meal component for new meals.

--- src/health/meals/meals.module.ts
+++ src/health/meals/meals.module.ts
@@ -7,8 +7,12 @@ import { RouterModule, Routes } from "@angular/router";
 import { SharedModule } from "../shared/shared.module";
 // containers
 import { MealsComponent } from "./containers/meals/meals.component";
+import { MealComponent } from "./containers/meal/meal.component";
 
-export const ROUTES: Routes = [{ path: "", component: MealsComponent }];
+export const ROUTES: Routes = [
+  { path: "", component: MealsComponent },
+  { path: "new", component: MealComponent },
+];
 @NgModule({
   imports: [
     CommonModule,
@@ -16,6 +20,6 @@ export const ROUTES: Routes = [{ path: "", component: MealsComponent }];
     RouterModule.forChild(ROUTES),
     SharedModule,
   ],
-  declarations: [MealsComponent],
+  declarations: [MealsComponent, MealComponent],
 })
 export class MealsModule {}
